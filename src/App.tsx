9ad8e90550072fa92497efa4dabe2de1348e4359
Yauhen <silent_control@tut.by>
2020-03-09 19:07:42 +0300

    Add functional component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
-const App = () => {
-  return (
-    <h1>Hello world!!</h1>
-  );
+// const Title:React.FC<{title: string}> = ({title, children}) => <h1>{title}{children}</h1>;
+
+type TitleProps = {
+  title: string,
+  test?: string,
 }
+
+const Title = ({title, test = 'test'}: TitleProps) => <h1>{title}{test}</h1>;
+
+const App = () => <Title title="test" />
 
 export default App;
