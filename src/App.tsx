2ca7cda98748487e7262c2e887c53d9595b40fe8
Yauhen <silent_control@tut.by>
2020-03-09 20:12:11 +0300

    Add Class component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,58 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-// const Title:React.FC<{title: string}> = ({title, children}) => <h1>{title}{children}</h1>;
-
-type TitleProps = {
-  title: string,
-  test?: string,
+type CounterState = {
+  count: number,
 }
 
-const Title = ({title, test = 'test'}: TitleProps) => <h1>{title}{test}</h1>;
+type CounterProps = {
+  title?: string,
+}
 
-const App = () => <Title title="test" />
+class Counter extends Component<CounterProps, CounterState> {
+  constructor(props: CounterProps) {
+    super(props)
+
+    this.state = {
+      count: 0,
+    }
+  }
+
+  static defaultProps: CounterProps = {
+    title: "Default counter: ",
+  }
+
+  static getDerivedStateFromProps(props: CounterProps, state: CounterState): CounterState | null {
+    return false ? { count: 2 } : null;
+  }
+
+  componentDidMount():void {
+
+  }
+
+  shouldComponentUpdate(nextProps: CounterProps, nextState: CounterState): boolean {
+    return true;
+  }
+
+  handleClick = () => {
+    this.setState(({ count }) => ({
+      count: ++count,
+    }));
+
+    // this.state.count = ++count; <-- Never do this
+  }
+
+  render() {
+    // this.props.title = ''; <-- And this...
+
+    return (
+      <div>
+        <h1>{this.props.title}{this.state.count}</h1>
+        <button onClick={this.handleClick}>+1</button>
+      </div>
+    );
+  }
+}
+
+const App = () => <Counter title="Counter: " />
 
 export default App;
