34d9d910f40393eb39a64364a8ade1f971a6c966
Yauhen <silent_control@tut.by>
2020-03-10 20:06:56 +0300

    Form elements (+ repeat previous topics)

--- src/App.tsx
+++ src/App.tsx
@@ -1,70 +1,129 @@
 import React, { Component } from 'react';
-/*
-type CounterState = {
-  count: number,
+
+type Position = {
+  id: string,
+  value: string,
+  title: string,
 }
 
-type CounterProps = {
-  title?: string, 
+type FormState = {
+  inputText: string,
+    textareaText: string,
+    selectText: string,
+    showData: {
+      name: string,
+      text: string,
+      position: string,
+    }
 }
 
-class Counter extends Component<CounterProps, CounterState> {
+const POSITIONS: Array<Position> = [
+  {
+    id: 'fd',
+    value: 'Front-end Developer',
+    title: 'Front-end Developer',
+  },
+  {
+    id: 'bd',
+    value: 'Back-end Developer',
+    title: 'Back-end Developer',
+  }
+];
+
+const DEFAULT_SELECT_VALUE:string = POSITIONS[0].value;
+const styles: React.CSSProperties = { display: 'block', marginBottom: '10px' };
+
+class Form extends Component<{}, FormState> {
+  
   state = {
-    count: 0,
+    inputText: '',
+    textareaText: '',
+    selectText: DEFAULT_SELECT_VALUE,
+    showData: {
+      name: '',
+      text: '',
+      position: '',
+    }
   }
 
-  handleClick = (e: React.MouseEvent<HTMLButtonElement | HTMLAnchorElement>) => {
-    console.log(`${e.clientX}, ${e.clientY}`);
-    this.setState(({ count }) => ({
-      count: ++count,
-    }));
-  }
+  private rootRef = React.createRef<HTMLSelectElement>();
 
-  render() {
-    return (
-      <div>
-        <h1>{this.props.title}{this.state.count}</h1>
-        <button onClick={this.handleClick}>+1</button>
-        <a href="#" onClick={this.handleClick}>Link</a>
-      </div>
-    );
-  }
-}
-*/
+  handleInputChange = (e: React.ChangeEvent<HTMLInputElement>):void => {
+    const { target: { value: inputText } } = e;
+    this.setState({ inputText });
+  };
 
-class Form extends Component<{}, {}> {
+  handleTextareaChange = (e: React.ChangeEvent<HTMLTextAreaElement>):void => {
+    const { target: { value: textareaText } } = e;
+    this.setState({ textareaText });
+  };
 
-  handleFocus = (e: React.FocusEvent<HTMLInputElement>) => {
-    console.log(e.currentTarget);
-  }
+  handleSelectChange = (e: React.ChangeEvent<HTMLSelectElement>):void => {
+    const { target: { value: selectText } } = e;
+    this.setState({ selectText });
+  };
 
-  handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  handleShow = (e: React.MouseEvent<HTMLButtonElement>):void => {
     e.preventDefault();
-    console.log('Submitted!');
-  }
+    const { inputText, textareaText, selectText } = this.state;
 
-  handleCopy = (e: React.ClipboardEvent<HTMLInputElement>) => {
-    console.log('Coppied!');
-  }
+    this.setState({
+      inputText: '',
+      textareaText: '',
+      selectText: DEFAULT_SELECT_VALUE,
+      showData: {
+        name: inputText,
+        text: textareaText,
+        position: selectText,
+      }
+    })
+  };
 
   render() {
+    const { inputText, textareaText, selectText, showData } = this.state;
+    const { name, text, position } = showData;
+
     return (
-      <form
-        onSubmit={this.handleSubmit}
-      >
-        <label>
-          Simple text:
-          <input
-            onFocus={this.handleFocus}
-            onCopy={this.handleCopy}
-            type="text"
-            name="text"
-          />
-          <button
-            type="submit"
-          >Submit</button>
-        </label>
-      </form>
+      <>
+        <form>
+          <label style={styles}>
+            Name:
+            <input
+              type="text"
+              value={inputText}
+              onChange={this.handleInputChange}
+            />
+          </label>
+
+          <label style={styles}>
+            Text:
+            <textarea
+              value={textareaText}
+              onChange={this.handleTextareaChange}
+            />
+          </label>
+
+          <select
+            style={styles}
+            value={selectText}
+            onChange={this.handleSelectChange}
+            ref={this.rootRef}
+          >
+            {POSITIONS.map(({ id, value, title }) => (
+              <option
+                key={id}
+                value={value}
+              >{title}</option>
+            ))}
+          </select>
+
+          <button onClick={this.handleShow}>Show Data</button>
+        </form>
+
+        <h2>{name}</h2>
+        <h3>{text}</h3>
+        <h3>{position}</h3>
+      </>
     );
   }
 }
