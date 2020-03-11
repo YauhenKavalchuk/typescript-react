eb37f7b48b6ba9eb51393f9238fe186bb9b8c90b
Yauhen <silent_control@tut.by>
2020-03-11 18:44:07 +0300

    HOCs

--- src/App.tsx
+++ src/App.tsx
@@ -1,77 +1,37 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
-/*
-// --------- useState ---------
-// Inferred as number
-const [value, setValue] = useState(0);
-
-// Explicitly setting the types
-const [value, setValue] = useState<number | undefined>(undefined);
-const [value, setValue] = useState<Array<number>>([]);
-
-interface IUser {
-  name: string;
-  age?: number;
-}
-const [value, setValue] = useState<IUser>({ name: 'Yauhen' });
-
-// --------- useRef ---------
-const ref1 = useRef<HTMLElement>(null!);
-const ref2 = useRef<HTMLElement | null>(null);
-
-// --------- useContext ---------
-interface ITheme {
-  backgroundColor: string;
-  color: string;
+type BaseProps = {
+  primTitle: string,
+  secTitle?: string,
 }
 
-// Context creation
-const ThemeContext = createContext<ITheme>({
-  backgroundColor: 'black',
-  color: 'white',
-})
+type InjectedProps = {
+  toggleStatus: Boolean,
+  toggle: () => void,
+}
 
-// Accessing context in a child component
-const themeContext = useContext<ITheme>(ThemeContext);
+const Button = ({ primTitle, secTitle, toggle, toggleStatus }: any) => (
+  <button onClick={toggle}>
+    {toggleStatus ? primTitle: secTitle}
+  </button>
+);
 
-// --------- useReducer ---------
+const withToggle = <BaseProps extends InjectedProps>(PassedComponent: React.ComponentType<BaseProps>) => {
+  return (props: BaseProps) => {
+    const [toggleStatus, toggle] = useState(false);
 
-interface State { count: number; }
-
-type Action = { type: 'increment' | 'decrement' }
-
-const counterReducer = ({ count }: State, { type }: Action) => {
-  switch (type) {
-    case 'increment': return { count: count + 1 };
-    case 'decrement': return { count: count - 1 };
-    default: return {};
+    return (
+      <PassedComponent
+        {...props as BaseProps}
+        toggle={() => toggle(!toggleStatus)}
+        toggleStatus={toggleStatus}
+      />
+    );
   }
-};
+}
 
-const [state, dispatch] = useReducer(counterReducer, { count: 0 });
+const ToogleButton = withToggle(Button);
 
-dispatch({ type: 'increment' });
-dispatch({ type: 'decrement' });
-
-// --------- useCallback & useMemo ---------
-// Callback
-// Inferred as number
-const memoizedCallback = useCallback(() => { sum(a, b); }, [a, b]);
-
-// Memo
-// Inferred as (value1: number, value2: number) => number
-const memoizedValue = useMemo((a: number, b: number) => sum(a, b), [a, b]);
-
-// --------- useEffect & useLayoutEffect ---------
-useEffect(() => {
-  const subscriber = subscribe(options);
-  return () => {
-    unsubscribe(subscriber)
-  };
-}, [options]);
-
-*/
-
-const App:React.FC = () => null;
+const App:React.FC = () => <ToogleButton primTitle="Maint Title" secTitle="Additional Title" />;
 
 export default App;
